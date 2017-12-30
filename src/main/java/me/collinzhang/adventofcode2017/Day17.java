8528bc8ed0c845722de635a7b306b745b49e5bb8
Collin <collinzh@users.noreply.github.com>
2017-12-30 18:43:14 -0500

    Update solution for day 17 part 2

--- src/main/java/me/collinzhang/adventofcode2017/Day17.java
+++ src/main/java/me/collinzhang/adventofcode2017/Day17.java
@@ -13,32 +13,24 @@ public class Day17 {
     }
 
     private static int day17P2(int input) {
-        LinkedList<Integer> spinlock = new LinkedList<>();
-        spinlock.add(0);
 
-        ListIterator<Integer> iterator = spinlock.listIterator(0);
+        int currentPosition = 0;
+        int totalLength = 1;
+        int number = 0;
 
-        for (int toInset = 1; toInset <= 50_000_000; toInset++) {
-            for (int i = 0; i < input; i++) {
-                iterator.next();
-                if (!iterator.hasNext() && i != input - 1) {
-                    iterator = spinlock.listIterator(0);
-                }
+        for (int i = 0; i < 50_000_000; i++) {
+            currentPosition += input;
+            currentPosition %= totalLength;
+            if (currentPosition == 0) {
+                number = i;
             }
-            iterator.add(toInset);
-            if (!iterator.hasNext()) {
-                iterator = spinlock.listIterator(0);
-            }
+
+            totalLength++;
+            currentPosition++;
+
         }
 
-        iterator = spinlock.listIterator(0);
-        for (; ; ) {
-            if (iterator.next() == 0) {
-                break;
-            }
-        }
-
-        return iterator.hasNext() ? iterator.next() : spinlock.get(0);
+        return number;
     }
 
     private static int day17P1(int input) {
